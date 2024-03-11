3754ab3a90aa9cc6a1bc118f19145adb4b284ccd
Muhammad-Juman-jarwar <muhammad1juman@gmail.com>
2024-03-12 04:03:48 +0500

    feat: created header and footer as per the design.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./pages/header/Header";
+import Footer from "./pages/footer/Footer";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Header />} />
+      <Route path="/contact" element={<Footer />} />
+    </Routes>
+  );
+}
+
+export default App;
